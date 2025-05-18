163b920ba3d17920f17afc3f1ec4f70688b8eefd
Harshit Kumar <kharshit242@gmail.com>
2025-05-18 23:54:24 +0530

    update

--- src/loaders.py
+++ src/loaders.py
@@ -1,8 +1,8 @@
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def load_and_split(pdf_path: str, chunk_size=1000, overlap=200):
-    loader = PyPDFLoader(pdf_path)
+    loader = PyMuPDFLoader(pdf_path)
     pages  = loader.load_and_split()
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size, chunk_overlap=overlap
